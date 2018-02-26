834c6a383fe7c020cce5d38a8f808e3607f3d44b
Nicolas Chapon <nchapon@gmail.com>
2018-02-26 21:46:05 +0100

    Set macOS defaults

---
+++ macos/set-defaults.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# From https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# Run ./set-defaults.sh and you'll be good to go.
+
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
