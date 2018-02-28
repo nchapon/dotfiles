3e30bac7f70f941f7161cff93242f5a9db483e17
nchapon <nchapon@gmail.com>
2018-02-28 19:03:41 +0100

    Add ls aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,2 +1,11 @@
 alias reload='. ~/.bashrc'
 alias cls='clear' # Good 'ol Clear Screen command
+
+
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
