98c09bf017dbcb4437caa600144bbbaffb9f8380
nchapon <nchapon@gmail.com>
2018-03-02 23:30:08 +0100

    Update macos defaults

--- Brewfile
+++ Brewfile
@@ -3,8 +3,12 @@ cask_args appdir: '/Applications'
 tap 'homebrew/bundle'
 
 brew 'coreutils'
+brew 'httpie'
+brew 'imagemagick'
 brew 'node'
+brew 'pandoc'
 brew 'the_silver_searcher'
+brew 'tree'
 brew 'wget'
 
 cask 'iterm2'
--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -17,6 +17,19 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+
+# Disable audible and visual bells
+defaults write com.apple.terminal "Bell" -bool false
+defaults write com.apple.terminal "VisualBell" -bool false
