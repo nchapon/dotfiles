0a8fecd1a476322a0fecf05a2c32ccbbb779c598
nchapon <nchapon@gmail.com>
2018-03-02 11:13:11 +0100

    Fix git bash prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -9,61 +9,6 @@
 # Heavily inspired by @necolas’s prompt: https://github.com/necolas/dotfiles
 # iTerm → Profiles → Text → use 13pt Monaco with 1.1 vertical spacing.
 
-# if [[ $COLORTERM = gnome-* && $TERM = xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
-#     export TERM='gnome-256color';
-# elif infocmp xterm-256color >/dev/null 2>&1; then
-#     export TERM='xterm-256color';
-# fi;
-
-prompt_git() {
-    local s='';
-    local branchName='';
-
-    # Check if the current directory is in a Git repository.
-    if [ $(git rev-parse --is-inside-work-tree &>/dev/null; echo "${?}") == '0' ]; then
-
-        # check if the current directory is in .git before running git checks
-        if [ "$(git rev-parse --is-inside-git-dir 2> /dev/null)" == 'false' ]; then
-
-            # Ensure the index is up to date.
-            git update-index --really-refresh -q &>/dev/null;
-
-            # Check for uncommitted changes in the index.
-            if ! $(git diff --quiet --ignore-submodules --cached); then
-                s+='+';
-            fi;
-
-            # Check for unstaged changes.
-            if ! $(git diff-files --quiet --ignore-submodules --); then
-                s+='!';
-            fi;
-
-            # Check for untracked files.
-            if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-                s+='?';
-            fi;
-
-            # Check for stashed files.
-            if $(git rev-parse --verify refs/stash &>/dev/null); then
-                s+='$';
-            fi;
-
-        fi;
-
-        # Get the short symbolic ref.
-        # If HEAD isn’t a symbolic ref, get the short SHA for the latest commit
-        # Otherwise, just give up.
-        branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
-            git rev-parse --short HEAD 2> /dev/null || \
-            echo '(unknown)')";
-
-        [ -n "${s}" ] && s=" [${s}]";
-
-        echo -e "${blue}(${orange}${branchName}${blue})${red}${s}";
-    else
-        return;
-    fi;
-}
 
 if tput setaf 1 &> /dev/null; then
     tput sgr0; # reset colors
@@ -96,4 +41,45 @@ else
 fi
 
 
-export PS1="\[\033[38;5;2m\]➜ \[$(tput sgr0)\] \w \[$(tput sgr0)\]\$(prompt_git) \[$(tput sgr0)\]"
+git=$(which git)
+
+git_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+git_prompt_info () {
+ ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
+}
+
+git_dirty() {
+
+  if $(! $git status -s &> /dev/null)
+  then
+    echo ""
+  else
+    if [[ $($git status --porcelain) == "" ]]
+    then
+      echo -e "on ${green}$(git_prompt_info)${reset}"
+    else
+      echo -e "on ${red}$(git_prompt_info)${reset}"
+    fi
+  fi
+}
+
+need_push () {
+  if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
+  then
+    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
+
+    if [[ $number == 0 ]]
+    then
+      echo " "
+    else
+      echo -e " with ${orange}$number unpushed${reset}"
+    fi
+  fi
+}
+
+export PS1=" \w \$(git_dirty)\$(need_push)\n \[${green}\]➜ \[${reset}\]"
