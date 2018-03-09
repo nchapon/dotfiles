63c9b206806255358b1a9b29c670b139705893d2
nchapon <nchapon@gmail.com>
2018-03-09 21:49:53 +0100

    Setup Macos bash completion

---
+++ bash/completion.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Bash completion
+
+_macos_completion () {
+    if [ -f $(brew --prefix)/etc/bash_completion ]; then
+        . $(brew --prefix)/etc/bash_completion
+    fi
+}
+
+_linux_completion () {
+    if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+        . /etc/bash_completion
+    fi
+}
+
+
+if is_osx; then
+  _macos_completion
+else
+  _linux_completion
+fi
