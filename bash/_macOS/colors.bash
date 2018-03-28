b71e151d787da088b675776e7bc0f34d8f9360ba
nchapon <nchapon@gmail.com>
2018-03-28 22:16:08 +0200

    Split colors by OS

---
+++ bash/_macOS/colors.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source $HOME/.dotfiles/env
+
+echo "macOS Colors"
+export CLICOLOR=1
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
--- bash/colors.bash
+++ bash/colors.bash
@@ -1,26 +1,4 @@
 #!/usr/bin/env bash
-
 source $HOME/.dotfiles/env
 
-_macos_colors () {
-    export CLICOLOR=1
-    export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
-}
-
-
-# enable color support of ls and also add handy aliases
-_linux_colors () {
-    if [ -x /usr/bin/dircolors ]; then
-        test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-        alias ls='ls --color=auto'
-        alias grep='grep --color=auto'
-        alias fgrep='fgrep --color=auto'
-        alias egrep='egrep --color=auto'
-    fi
-}
-
-if is_macOS; then
-  _macos_colors
-else
-  _linux_colors
-fi
+_source _$(get_os)/colors.bash
