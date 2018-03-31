0329f24757fb3e5a5eb67190b90df507b839524b
nchapon <nchapon@gmail.com>
2018-03-31 17:42:12 +0200

    Improve bash config files

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,12 +1,9 @@
-# cd "$(dirname "${BASH_SOURCE[0]}")" \
-#     && . "../env"
-
-declare -r CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+     && . "../env"
 
 alias reload='. ~/.bashrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias cdp='cd ~/Projects'
 
-_source ${CURRENT_DIR}/_$(get_os)/aliases.bash
+_source _$(get_os)/aliases.bash
