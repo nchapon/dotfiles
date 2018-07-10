4463330069f5a48ef8955ef6e99ce7a14bc31ab7
nchapon <nchapon@gmail.com>
2018-07-10 17:33:42 +0200

    Optimize bash history

--- bash/options.bash
+++ bash/options.bash
@@ -2,11 +2,22 @@
 
 # don't put duplicate lines in the history. See bash(1) for more options
 # ... or force ignoredups and ignorespace
-HISTCONTROL=ignoredups:ignorespace
+export HISTCONTROL=ignoredups:ignorespace
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+export HISTSIZE=1000
+export HISTFILESIZE=2000
+
+# Use one command per line
+shopt -s cmdhist
+
+
+# Exclude ls, job controls and history itself
+export HISTIGNORE='ls:bg:fg:history'
+
+
+# Store history immediatly
+export PROMPT_COMMAND='history -a'
