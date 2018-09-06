9ca0903df88d539ec1f4fcbdca7bd1370c25fe96
nchapon <nchapon@gmail.com>
2018-09-06 12:27:04 +0200

    Add magit alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -5,5 +5,7 @@ alias reload='. ~/.bashrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias cdp='cd ~/Projects'
+alias magit='emacsclient -a emacs -e "(progn (magit-status \"$(git rev-parse --show-toplevel)\") (delete-other-windows))"'
+
 
 _source _$(get_os)/aliases.bash
