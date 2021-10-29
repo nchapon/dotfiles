43412ca2d8f15151ed724f18a8769a36c10f7517
nchapon <nchapon@gmail.com>
2021-10-29 10:44:13 +0200

    Improve org-speed-command behavior & move heading

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -314,6 +314,63 @@
 (if (fboundp 'fringe-mode)
     (fringe-mode 2))
 
+(use-package ace-window
+  :bind (([remap other-window] . ace-window))
+  :config
+  (setq aw-keys '(?q ?s ?d ?f ?g ?h ?j ?k ?l))
+  ;; increase size face
+  (custom-set-faces
+   '(aw-leading-char-face
+     ((t (:inherit ace-jump-face-foreground :height 3.0))))))
+
+(use-package golden-ratio  
+  :diminish t
+  :init
+  (golden-ratio-mode 1)
+  :config
+  (setq golden-ratio-extra-commands
+        (append golden-ratio-extra-commands
+                '(ace-window))))
+
+(defun nc/toggle-split-window-horizontally ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+(bind-key "C-z w  -" 'nc/toggle-split-window-horizontally)
+
+(defun nc--split-window-right-and-move-there-dammit ()
+  (split-window-right)
+  (windmove-right))
+
+(defun nc/toggle-split-window-vertically ()
+  (interactive)
+  (if (> (count-windows) 1)
+      (delete-other-windows)
+    (nc--split-window-right-and-move-there-dammit)))
+
+(bind-key "C-z w +" 'nc/toggle-split-window-vertically)
+
 (use-package selectrum
   :init (selectrum-mode +1))
 (use-package prescient
@@ -492,63 +549,6 @@
   ;; auto-updating embark collect buffer
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
-
-(use-package ace-window
-  :bind (([remap other-window] . ace-window))
-  :config
-  (setq aw-keys '(?q ?s ?d ?f ?g ?h ?j ?k ?l))
-  ;; increase size face
-  (custom-set-faces
-   '(aw-leading-char-face
-     ((t (:inherit ace-jump-face-foreground :height 3.0))))))
-
-(use-package golden-ratio  
-  :diminish t
-  :init
-  (golden-ratio-mode 1)
-  :config
-  (setq golden-ratio-extra-commands
-        (append golden-ratio-extra-commands
-                '(ace-window))))
-
-(defun nc/toggle-split-window-horizontally ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
-
-(bind-key "C-z w  -" 'nc/toggle-split-window-horizontally)
-
-(defun nc--split-window-right-and-move-there-dammit ()
-  (split-window-right)
-  (windmove-right))
-
-(defun nc/toggle-split-window-vertically ()
-  (interactive)
-  (if (> (count-windows) 1)
-      (delete-other-windows)
-    (nc--split-window-right-and-move-there-dammit)))
-
-(bind-key "C-z w +" 'nc/toggle-split-window-vertically)
 
 (use-package magit
 
@@ -1209,10 +1209,9 @@
     (org-show-entry)
     (show-children)))
 
-(setq org-use-speed-commands t)
-
-;; (setq org-use-speed-commands
-;;       (lambda () (and (looking-at org-outline-regexp) (looking-back "^\**"))))
+;; Improve speed command behavior
+(setq org-use-speed-commands
+       (lambda () (and (looking-at org-outline-regexp) (looking-back "^\**"))))
 
 (push '("N" nc/org-show-next-heading-tidily) org-speed-commands)
 (push '("P" nc/org-show-previous-heading-tidily) org-speed-commands)
