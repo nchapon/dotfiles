43845ad192752766ec15827509736e41893433bd
nchapon <nchapon@gmail.com>
2021-12-17 18:17:38 +0100

    [emacs] Treemacs binding

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -643,7 +643,7 @@
   :defer t
   :init
   (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+    (define-key winum-keymap (kbd "M-o") #'treemacs-select-window))
   :config
   (progn
     (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
