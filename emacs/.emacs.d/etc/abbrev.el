4660290a9edd50c2f83cb1c264190ca781539b40
nchapon <nchapon@gmail.com>
2022-05-11 11:08:34 +0200

    Add new abbrevs

--- emacs/.emacs.d/etc/abbrev.el
+++ emacs/.emacs.d/etc/abbrev.el
@@ -2,7 +2,8 @@
 (define-abbrev-table 'org-mode-abbrev-table
   '(
     ("arch" "architecture" nil :count 1)
-    ("cb" "[/]" nil :count 1)
+    ("cb" "[/]" nil :count 2)
+    ("dm" "Daily Meeting" nil :count 0)
     ("dom" "domaine" nil :count 0)
     ("fc" "Fondation Cloud" nil :count 0)
     ("mo" "monolithe" nil :count 4)
