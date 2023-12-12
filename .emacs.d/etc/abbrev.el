07c67e066c98437c495cb95c3f681b593cf8792a
nchapon <nchapon@gmail.com>
2023-12-12 17:58:46 +0100

    Remove _ in abbrevs

--- .emacs.d/etc/abbrev.el
+++ .emacs.d/etc/abbrev.el
@@ -1,11 +1,10 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'org-mode-abbrev-table
   '(
+    ("10h" "Lundi 10h" nil :count 1)
     ("11" "One To One" nil :count 2)
     ("cb" "[/]" nil :count 2)
-    ("_10h" "Lundi 10h" nil :count 0)
-    
-    ("_ops" "DevOps Sync" nil :count 1)
-    ("_ssp" "Weekly SSP" nil :count 1)
+    ("ops" "DevOps Sync" nil :count 1)
+    ("ssp" "Weekly SSP" nil :count 1)
    ))
 
